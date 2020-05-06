290bd57237a6cd72234d15567bc973113b82ab3b
Adam Alenius <adam.alenius@gmail.com>
2020-05-06 21:18:36 +0200

    add typings for OMDb results

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -13,3 +13,36 @@ declare module '*.module.css' {
   const cssModule: CSSModule
   export = cssModule
 }
+
+interface OMDbRating {
+  Source: string
+  Value: string
+}
+
+interface OMDbResult {
+  Title: string
+  Year: string
+  Rated: string
+  Released: string
+  Runtime: string
+  Genre: string
+  Director: string
+  Writer: string
+  Actors: string
+  Plot: string
+  Language: string
+  Country: string
+  Awards: string
+  Poster: string
+  Ratings: OMDbRating[]
+  Metascore: string
+  imdbRating: string
+  imdbVotes: string
+  imdbID: string
+  Type: string
+  DVD: string
+  BoxOffice: string
+  Production: string
+  Website: string
+  Response: string
+}
