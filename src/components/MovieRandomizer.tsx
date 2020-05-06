60f3638871a7249286631048ba3385ead654d327
Adam Alenius <adam.alenius@gmail.com>
2020-05-06 21:21:11 +0200

    Add basic component that displays the movie you searched for

---
+++ src/components/MovieRandomizer.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, FormEvent, ChangeEvent } from 'react'
+import { useFetch } from '../hooks/useFetch'
+
+const baseUrl = process.env.GATSBY_BASE_URL
+
+const MovieRandomizer = () => {
+  const [movie, setMovie] = useState(encodeURIComponent('Pulp Fiction'))
+  const [movieInput, setMovieInput] = useState('')
+  const { response, error: fetchError } = useFetch(`${baseUrl}+&t=${encodeURIComponent(movie)}`, { method: 'GET' })
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault()
+    setMovie(movieInput)
+    setMovieInput('')
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    e.preventDefault()
+    setMovieInput(e.target.value)
+  }
+
+  return (
+    <div>
+      <p>wow random</p>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input placeholder="Movie title" title="Movie to search for" onChange={e => handleChange(e)} />
+      </form>
+      <img src={response?.Poster} alt="movie-poster" />
+      <p>{response ? response?.Title : fetchError}</p>
+    </div>
+  )
+}
+
+export default MovieRandomizer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
-import * as React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'gatsby'
 
 import Page from '../components/Page'
 import Container from '../components/Container'
 import IndexLayout from '../layouts'
+import MovieRandomizer from '../components/MovieRandomizer'
 
-const IndexPage = () => (
-  <IndexLayout>
-    <Page>
-      <Container>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
-      </Container>
-    </Page>
-  </IndexLayout>
-)
+const IndexPage = () => {
+  const [movie] = useState('The Green Mile')
+
+  return (
+    <IndexLayout>
+      <Page>
+        <Container>
+          <p>Welcome to my cool site.</p>
+          <MovieRandomizer />
+        </Container>
+      </Page>
+    </IndexLayout>
+  )
+}
 
 export default IndexPage
