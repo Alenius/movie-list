13b6f48cf3a1bbaeb0b6835b590155278e5598be
Alenius <32702699+Alenius@users.noreply.github.com>
2020-05-18 21:58:06 +0200

    add api call for movie poster (#5)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect, FormEvent, ChangeEvent } from "react";
 import imdbTopList from "./assets/imdb_top.json";
 import "./App.css";
 import { Button } from "./components/Button";
+import { useFetch } from "./hooks/useFetch";
+
+const BACKEND_ENDPOINT_BASE =
+  "https://aa-movie-list-backend.herokuapp.com/movie?title=";
 
 export const getRandomInt = (maxValue) =>
   Math.floor(Math.random() * Math.floor(maxValue));
@@ -11,12 +15,24 @@ const MovieRandomizer = () => {
   const [movie, setMovie] = useState(encodeURIComponent("Pulp Fiction"));
   const [movieArray, setMovieArray] = useState([]);
   const [randomIndex, setRandomIndex] = useState(0);
+  const {
+    response,
+    error: fetchError,
+  } = useFetch(
+    `${BACKEND_ENDPOINT_BASE}=${encodeURIComponent(movie.movieTitle)}`,
+    { method: "GET" }
+  );
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log("new movie arr", imdbTopList);
     setMovieArray(imdbTopList.movieArray);
   }, []);
+
+  useEffect(() => {
+    if (response) {
+      setMovie({ ...movie, posterLink: response.Poster });
+    }
+  }, [response]);
 
   useEffect(() => {
     if (movieArray.length) {
@@ -34,6 +50,18 @@ const MovieRandomizer = () => {
     <div className="base-div">
       <h1 className="title">MovieRandomizer</h1>
       <div className="container">
+        <div className="img-container">
+          {loading ? (
+            <p>Loading poster...</p>
+          ) : (
+            <img
+              src={movie?.posterLink}
+              alt="movie-poster"
+              onLoad={() => setLoading(false)}
+              onError={() => setLoading(true)}
+            />
+          )}
+        </div>
         <h2 style={{ textAlign: "center" }}>{movie?.movieTitle}</h2>
         <div className="rank-rate-div">
           <p className="rank-rate-p">
